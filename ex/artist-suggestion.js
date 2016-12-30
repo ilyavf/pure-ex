1ccb6c47a6f91052d9d20c10602591a270f9b23c
Ilya Fadeev <fadeev.ilya@gmail.com>
2016-12-29 19:24:45 -0500

    related-artists: fetch real data

--- ex/artist-suggestion.js
+++ ex/artist-suggestion.js
@@ -7,15 +7,26 @@
 // related: artists
 
 const Task = require('data.task');
+const request = require('request');
 
 const getArgs = new Task((rej, res) => res(process.argv));
 const names = getArgs.map(args => args.slice(2));
 
+const httpGet = url =>
+  new Task((rej, res) => {
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res(JSON.parse(body));
+      }
+    })
+  });
+
 const findArtist = name =>
-  new Task((rej, res) => res({artists: {items: {id: 1}}}));
+  httpGet('https://api.spotify.com/v1/search?type=artist&q=' + name)
+    .map(r => r.artists.items[0]);
 
 const relatedArtists = id =>
-  new Task((rej, res) => res([{artists:[{name:'One'},{name:'Two'}]}]));
+  new Task((rej, res) => res([{id: id, artists:[{name:'One'},{name:'Two'}]}]));
 
 const related = name =>
   findArtist(name)
@@ -29,4 +40,4 @@ const main = ([name1, name2]) =>
 
 
 
-names.map(main).fork(console.err, console.log);
+names.chain(main).fork(console.err, console.log);
